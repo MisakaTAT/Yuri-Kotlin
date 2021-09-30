1cef1f0a69e0d989361ff46f64d604c19a6cedff
MisakaTAT <i@mikuac.com>
2021-09-30 22:18:43 +0800

    feat: add tencent bot filter

---
+++ src/main/kotlin/com/mikuac/yuri/plugins/filter/TencentBotFilter.kt
@@ -0,0 +1,21 @@
+package com.mikuac.yuri.plugins.filter
+
+import com.mikuac.shiro.core.Bot
+import com.mikuac.shiro.core.BotPlugin
+import com.mikuac.shiro.dto.event.message.GroupMessageEvent
+import com.mikuac.yuri.common.log.Slf4j.Companion.log
+
+class TencentBotFilter : BotPlugin() {
+
+    override fun onGroupMessage(bot: Bot, event: GroupMessageEvent): Int {
+        val userId = event.userId
+        val startId = 2854196300L
+        val endId = 2854216399L
+        if (userId in startId..endId) {
+            log.info("已拦截QQ官方机器人消息 内容：${event.message}")
+            return MESSAGE_BLOCK
+        }
+        return MESSAGE_IGNORE
+    }
+
+}
