3cef553c70049e8f26ebb5f8f8017207ca747a8f
MisakaTAT <i@mikuac.com>
2021-12-14 12:52:32 +0800

    chore: optimze epic free game plugin

---
+++ src/main/kotlin/com/mikuac/yuri/dto/EpicDto.kt
@@ -0,0 +1,64 @@
+package com.mikuac.yuri.dto
+
+class EpicDto : ArrayList<EpicDto.EpicDtoItem>() {
+    data class EpicDtoItem(
+        val customAttributes: List<CustomAttribute>,
+        val description: String,
+        val effectiveDate: String,
+        val id: String,
+        val keyImages: List<KeyImage>,
+        val price: Price,
+        val promotions: Promotions,
+        val seller: Seller,
+        val title: String,
+        val url: Any,
+        val urlSlug: String
+    ) {
+        data class CustomAttribute(
+            val key: String,
+            val value: String
+        )
+
+        data class KeyImage(
+            val type: String,
+            val url: String
+        )
+
+        data class Price(
+            val totalPrice: TotalPrice
+        ) {
+            data class TotalPrice(
+                val fmtPrice: FmtPrice,
+            ) {
+                data class FmtPrice(
+                    val originalPrice: String
+                )
+            }
+
+        }
+
+        data class Promotions(
+            val promotionalOffers: List<PromotionalOffers>,
+            val upcomingPromotionalOffers: List<UpcomingPromotionalOffer>
+        ) {
+            data class PromotionalOffers(
+                val promotionalOffers: List<PromotionalOffer>
+            )
+
+            data class UpcomingPromotionalOffer(
+                val promotionalOffers: List<PromotionalOffer>
+            )
+
+            data class PromotionalOffer(
+                val startDate: String,
+                val endDate: String
+            )
+        }
+
+        data class Seller(
+            val id: String,
+            val name: String
+        )
+
+    }
+}
--- src/main/kotlin/com/mikuac/yuri/plugins/aop/EpicFreeGame.kt
+++ src/main/kotlin/com/mikuac/yuri/plugins/aop/EpicFreeGame.kt
@@ -4,14 +4,14 @@ import cn.hutool.core.date.DateField
 import cn.hutool.core.date.DateUtil
 import com.alibaba.fastjson.JSONArray
 import com.alibaba.fastjson.JSONObject
-import com.google.gson.JsonArray
-import com.google.gson.JsonObject
+import com.google.gson.Gson
 import com.google.gson.JsonParser
 import com.mikuac.shiro.common.utils.MsgUtils
 import com.mikuac.shiro.core.Bot
 import com.mikuac.shiro.core.BotPlugin
 import com.mikuac.shiro.dto.event.message.GroupMessageEvent
 import com.mikuac.yuri.config.ReadConfig
+import com.mikuac.yuri.dto.EpicDto
 import com.mikuac.yuri.enums.RegexEnum
 import com.mikuac.yuri.utils.DateUtils
 import com.mikuac.yuri.utils.LogUtils
@@ -25,7 +25,7 @@ import java.util.concurrent.TimeUnit
 @Component
 class EpicFreeGame : BotPlugin() {
 
-    private val expiringMap: ExpiringMap<String, JsonArray> = ExpiringMap.builder()
+    private val expiringMap: ExpiringMap<String, EpicDto> = ExpiringMap.builder()
         .variableExpiration()
         .expirationPolicy(ExpirationPolicy.CREATED)
         .expiration(ReadConfig.config.plugin.epic.cacheTime.times(1000L), TimeUnit.MILLISECONDS)
@@ -34,7 +34,7 @@ class EpicFreeGame : BotPlugin() {
     private val graphqlQuery =
         "query searchStoreQuery(${'$'}allowCountries: String, ${'$'}category: String, ${'$'}count: Int, ${'$'}country: String!, ${'$'}keywords: String, ${'$'}locale: String, ${'$'}namespace: String, ${'$'}sortBy: String, ${'$'}sortDir: String, ${'$'}start: Int, ${'$'}tag: String, ${'$'}withPrice: Boolean = false, ${'$'}withPromotions: Boolean = false) {\n Catalog {\n searchStore(allowCountries: ${'$'}allowCountries, category: ${'$'}category, count: ${'$'}count, country: ${'$'}country, keywords: ${'$'}keywords, locale: ${'$'}locale, namespace: ${'$'}namespace, sortBy: ${'$'}sortBy, sortDir: ${'$'}sortDir, start: ${'$'}start, tag: ${'$'}tag) {\n elements {\n title\n id\n namespace\n description\n effectiveDate\n keyImages {\n type\n url\n }\n seller {\n id\n name\n }\n productSlug\n urlSlug\n url\n items {\n id\n namespace\n }\n customAttributes {\n key\n value\n }\n price(country: ${'$'}country) @include(if: ${'$'}withPrice) {\n totalPrice {\n discountPrice\n originalPrice\n voucherDiscount\n discount\n currencyCode\n currencyInfo {\n decimals\n }\n fmtPrice(locale: ${'$'}locale) {\n originalPrice\n discountPrice\n intermediatePrice\n }\n }\n lineOffers {\n appliedRules {\n id\n endDate\n discountSetting {\n discountType\n }\n }\n }\n }\n promotions(category: ${'$'}category) @include(if: ${'$'}withPromotions) {\n promotionalOffers {\n promotionalOffers {\n startDate\n endDate\n discountSetting {\n discountType\n discountPercentage\n }\n }\n }\n upcomingPromotionalOffers {\n promotionalOffers {\n startDate\n endDate\n discountSetting { discountType\n discountPercentage\n }\n }\n }\n }\n }\n  }\n }\n}\n"
 
-    private fun doRequest(): JsonArray? {
+    private fun doRequest(): EpicDto? {
         val variables = JSONObject()
         variables["allowCountries"] = "CN"
         variables["category"] = "freegames"
@@ -57,18 +57,17 @@ class EpicFreeGame : BotPlugin() {
         val result = RequestUtils.post(
             "https://www.epicgames.com/store/backend/graphql-proxy", json.toJSONString(), headers
         )
+
+        // 检查缓存
         if (expiringMap["cache"] != null) {
             return expiringMap["cache"]
         }
         val jsonObject = JsonParser.parseString(result)
-        try {
-            val data = jsonObject.asJsonObject["data"].asJsonObject["Catalog"].asJsonObject["searchStore"]
-                .asJsonObject["elements"].asJsonArray ?: return null
-            expiringMap["cache"] = data
-            return data
-        } catch (e: Exception) {
-            throw Exception("Epic 免费游戏获取失败")
-        }
+        val elements = jsonObject.asJsonObject["data"].asJsonObject["Catalog"].asJsonObject["searchStore"]
+            .asJsonObject["elements"].asJsonArray ?: throw Exception("Epic 免费游戏获取失败")
+        val data = Gson().fromJson(elements, EpicDto::class.java)
+        expiringMap["cache"] = data
+        return data
     }
 
     private fun formatDate(rawDate: String): String {
@@ -86,48 +85,37 @@ class EpicFreeGame : BotPlugin() {
                 // 取最新的三条
                 if (i > 3) break
 
-                var promotionalOffers = JsonObject()
-                val gamePromotions = game.asJsonObject["promotions"].asJsonObject["promotionalOffers"].asJsonArray
-                val upcomingPromotions =
-                    game.asJsonObject["promotions"].asJsonObject["upcomingPromotionalOffers"].asJsonArray
-                if (gamePromotions.size() != 0) promotionalOffers = gamePromotions[0].asJsonObject["promotionalOffers"]
-                    .asJsonArray[0].asJsonObject
-                if (upcomingPromotions.size() != 0) promotionalOffers = upcomingPromotions[0]
-                    .asJsonObject["promotionalOffers"].asJsonArray[0].asJsonObject
-                val startDate = formatDate(promotionalOffers.asJsonObject["startDate"].asString)
-                val endDate = formatDate(promotionalOffers.asJsonObject["endDate"].asString)
+                val pons = game.promotions.promotionalOffers
+                val upon = game.promotions.upcomingPromotionalOffers
+                val gameName = game.title
+                val gameDesc = game.description
+                val gamePrice = game.price.totalPrice.fmtPrice.originalPrice
 
-                val gameName = game.asJsonObject["title"].asString
-                val gameDesc = game.asJsonObject["description"].asString
-                // val gameCorp = game.asJsonObject["seller"].asJsonObject["name"].asString
-                val gamePrice = game.asJsonObject["price"].asJsonObject["totalPrice"]
-                    .asJsonObject["fmtPrice"].asJsonObject["originalPrice"].asString
-
-                val image = game.asJsonObject["keyImages"].asJsonArray.filter {
-                    "Thumbnail" == it.asJsonObject["type"].asString || "VaultClosed" == it.asJsonObject["type"].asString
+                val image = game.keyImages.filter {
+                    "Thumbnail" == it.type || "VaultClosed" == it.type
                 }
-                var imageUrl = ""
-                if (image.isNotEmpty()) imageUrl = image[0].asJsonObject["url"].asString
+                var imageUrl: String? = null
+                if (image.isNotEmpty()) imageUrl = image[0].url
 
-                val publisherName: String = game.asJsonObject["customAttributes"].asJsonArray.filter {
-                    it.asJsonObject["key"].asString == "publisherName"
-                }[0].asJsonObject["value"].asString
-                val developerName: String = game.asJsonObject["customAttributes"].asJsonArray.filter {
-                    it.asJsonObject["key"].asString == "developerName"
-                }[0].asJsonObject["value"].asString
+                val publisherName: String = game.customAttributes.filter { it.key == "publisherName" }[0].value
+                val developerName: String = game.customAttributes.filter { it.key == "developerName" }[0].value
 
-                val gamePage = "https://www.epicgames.com/store/zh-CN/p/${game.asJsonObject["urlSlug"].asString}"
+                val gamePage = "https://www.epicgames.com/store/zh-CN/p/${game.urlSlug}"
 
                 val msg = MsgUtils.builder()
 
-                if (upcomingPromotions.size() != 0) {
-                    if (imageUrl.isNotEmpty()) msg.img(imageUrl)
+                if (upon.isNotEmpty()) {
+                    val startDate = formatDate(upon[0].promotionalOffers[0].startDate)
+                    val endDate = formatDate(upon[0].promotionalOffers[0].endDate)
+                    if (imageUrl != null) msg.img(imageUrl)
                     msg.text("\n即将解锁：$gameName")
                     msg.text("\n限免时间为 $startDate 到 $endDate")
                 }
 
-                if (gamePromotions.size() != 0) {
-                    if (imageUrl.isNotEmpty()) msg.img(imageUrl)
+                if (pons.isNotEmpty()) {
+                    val startDate = formatDate(pons[0].promotionalOffers[0].startDate)
+                    val endDate = formatDate(pons[0].promotionalOffers[0].endDate)
+                    if (imageUrl != null) msg.img(imageUrl)
                     msg.text("\n${gameName}（${gamePrice}）")
                     msg.text("\n\n${gameDesc}")
                     msg.text("\n\n该游戏由 $developerName 制作，并由 $publisherName 发行。")
