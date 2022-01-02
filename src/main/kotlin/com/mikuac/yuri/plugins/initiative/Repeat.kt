4708e301b4127aa155f00159abd1ef2afc711b75
Zero <i@mikuac.com>
2022-01-02 22:50:22 +0800

    update: refactoring code for repeat plugin

--- src/main/kotlin/com/mikuac/yuri/plugins/initiative/Repeat.kt
+++ src/main/kotlin/com/mikuac/yuri/plugins/initiative/Repeat.kt
@@ -1,6 +1,7 @@
 package com.mikuac.yuri.plugins.initiative
 
 import cn.hutool.core.util.RandomUtil
+import com.mikuac.shiro.annotation.GroupMessageHandler
 import com.mikuac.shiro.core.Bot
 import com.mikuac.shiro.core.BotPlugin
 import com.mikuac.shiro.dto.event.message.GroupMessageEvent
@@ -13,52 +14,50 @@ import java.util.concurrent.TimeUnit
 @Component
 class Repeat : BotPlugin() {
 
-    /**
-     * 创建缓存 过期时间内不复读重复内容
-     */
     private val expiringMap: ExpiringMap<Long, String> = ExpiringMap.builder()
         .variableExpiration()
         .expirationPolicy(ExpirationPolicy.CREATED)
         .expiration(ReadConfig.config.plugin.repeat.waitTime.times(1000L), TimeUnit.MILLISECONDS)
         .build()
 
-    /**
-     * 最后一条消息
-     */
     private val lastMsgMap: HashMap<Long, String> = HashMap()
 
-    /**
-     * 消息统计
-     */
-    private val countMap: HashMap<Long, Int> = HashMap()
+    private val lastUserMap: HashMap<Long, Long> = HashMap()
 
-    override fun onGroupMessage(bot: Bot, event: GroupMessageEvent): Int {
+    private val msgCountMap: HashMap<Long, Int> = HashMap()
+
+    @GroupMessageHandler
+    fun repeatHandler(bot: Bot, event: GroupMessageEvent) {
         val msg = event.message
         val groupId = event.groupId
+        val userId = event.userId
 
         // 如果缓存中存在内容则不进行复读
         val cache = expiringMap[groupId]
-        if (cache != null && cache == msg) return MESSAGE_IGNORE
+        if (cache != null && cache == msg) return
 
-        // 获取Map中当前群组最后一条消息内容
+        // 如果最后一个用户是同一人则跳过
+        if (lastUserMap.getOrDefault(groupId, 0) == userId) return
+
+        var count = msgCountMap.getOrDefault(groupId, 0)
         val lastMsg = lastMsgMap.getOrDefault(groupId, "")
 
-        // 获取当前群组同内容消息重复次数
-        var count = countMap.getOrDefault(groupId, 0)
-
-        if (msg.equals(lastMsg)) {
-            countMap[groupId] = ++count
+        if (lastMsg == msg) {
+            lastMsgMap[groupId] = msg
+            lastUserMap[groupId] = userId
+            msgCountMap[groupId] = ++count
             if (count == RandomUtil.randomInt(ReadConfig.config.plugin.repeat.thresholdValue)) {
                 bot.sendGroupMsg(groupId, msg, false)
                 val waitTime = ReadConfig.config.plugin.repeat.waitTime.times(1000L)
                 expiringMap.put(groupId, msg, waitTime, TimeUnit.MILLISECONDS)
-                countMap[groupId] = 0
+                msgCountMap[groupId] = 0
             }
-        } else {
-            lastMsgMap[groupId] = msg
-            countMap[groupId] = 0
+            return
         }
-        return MESSAGE_IGNORE
+
+        lastMsgMap[groupId] = msg
+        msgCountMap[groupId] = 1
+        lastUserMap[groupId] = userId
     }
 
 }
