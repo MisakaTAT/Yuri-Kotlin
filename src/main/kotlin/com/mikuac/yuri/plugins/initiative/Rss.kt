05b2d53822fe26652808d39b290805ae286f9aff
MisakaTAT <i@mikuac.com>
2023-03-31 09:22:57 +0800

    fix: rss

--- src/main/kotlin/com/mikuac/yuri/plugins/initiative/Rss.kt
+++ src/main/kotlin/com/mikuac/yuri/plugins/initiative/Rss.kt
@@ -9,8 +9,6 @@ import com.mikuac.yuri.utils.DateUtils
 import com.mikuac.yuri.utils.NetUtils
 import com.rometools.rome.io.SyndFeedInput
 import com.rometools.rome.io.XmlReader
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.ApplicationArguments
 import org.springframework.boot.ApplicationRunner
@@ -28,37 +26,36 @@ class Rss : ApplicationRunner {
     private val cfg = Config.plugins.rss
 
     override fun run(args: ApplicationArguments?) {
-        start()
+        val thread = Thread(this::start)
+        thread.start()
     }
 
-    private fun start() = runBlocking {
-        launch {
-            log.info("RSS 监听线程已启动")
-            var lastCheckTime = Instant.now()
-            while (true) {
-                val currentTime = Instant.now()
-                val elapsed = Duration.between(lastCheckTime, currentTime)
-                if (elapsed.toMinutes() < cfg.check) {
-                    continue
-                }
-                cfg.urls.forEach { url ->
-                    val feed = NetUtils.get(url).body?.byteStream().use { xml ->
-                        SyndFeedInput().build(XmlReader(xml))
-                    }
-                    if (!feed.publishedDate.toInstant().isAfter(lastCheckTime)) return@forEach
-                    val item = feed.entries.first()
-                    val msg = MsgUtils.builder()
-                        .text("【RSS Subscribe】")
-                        .text("\n标题：${item.title}")
-                        .text("\n时间：${DateUtils.format(item.publishedDate)}")
-                        .text("\n链接：${item.link}")
-                        .build()
-                    cfg.groups.forEach {
-                        global.bot().sendGroupMsg(it, msg, false)
-                    }
-                }
-                lastCheckTime = currentTime
+    private fun start() {
+        log.info("RSS 监听线程已启动")
+        var lastCheckTime = Instant.now()
+        while (true) {
+            val currentTime = Instant.now()
+            val elapsed = Duration.between(lastCheckTime, currentTime)
+            if (elapsed.toMinutes() < cfg.check) {
+                continue
             }
+            cfg.urls.forEach { url ->
+                val feed = NetUtils.get(url).body?.byteStream().use { xml ->
+                    SyndFeedInput().build(XmlReader(xml))
+                }
+                if (!feed.publishedDate.toInstant().isAfter(lastCheckTime)) return@forEach
+                val item = feed.entries.first()
+                val msg = MsgUtils.builder()
+                    .text("【RSS Subscribe】")
+                    .text("\n标题：${item.title}")
+                    .text("\n时间：${DateUtils.format(item.publishedDate)}")
+                    .text("\n链接：${item.link}")
+                    .build()
+                cfg.groups.forEach {
+                    global.bot().sendGroupMsg(it, msg, false)
+                }
+            }
+            lastCheckTime = currentTime
         }
     }
 
