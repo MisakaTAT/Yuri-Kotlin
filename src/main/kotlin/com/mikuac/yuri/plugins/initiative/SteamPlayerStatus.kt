c481dad830e0300bd063b1e763df5d459160a74a
MisakaTAT <i@mikuac.com>
2024-03-29 16:16:01 +0800

    feat: added playing time

--- src/main/kotlin/com/mikuac/yuri/plugins/initiative/SteamPlayerStatus.kt
+++ src/main/kotlin/com/mikuac/yuri/plugins/initiative/SteamPlayerStatus.kt
@@ -10,6 +10,8 @@ import com.mikuac.yuri.utils.NetUtils
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
+import java.time.Duration
+import java.time.LocalDateTime
 
 @Slf4j
 @Shiro
@@ -20,10 +22,13 @@ class SteamPlayerStatus {
 
     private val cache = HashMap<String, String>()
 
+    private val gamingTime = HashMap<String, LocalDateTime>()
+
     @Autowired
     private lateinit var global: Global
 
-    private fun request(playerId: String): String {
+    private fun request(groupId: String, playerId: String): String {
+        val flag = groupId + playerId
         val domain = "https://api.steampowered.com"
         val api = "${domain}/ISteamUser/GetPlayerSummaries/v0002/?key=${cfg.apiKey}&steamids=${playerId}"
         NetUtils.get(api).use { resp ->
@@ -33,23 +38,26 @@ class SteamPlayerStatus {
             val gameName = players.asJsonObject["gameextrainfo"]
             when {
                 // 如果发现开始玩了而之前未玩
-                gameName != null && cache[playerId] == null -> {
-                    cache[playerId] = gameName.asString
-                    return "[Steam] $personaName 正在游玩 ${cache[playerId]}"
+                gameName != null && cache[flag] == null -> {
+                    cache[flag] = gameName.asString
+                    gamingTime[flag] = LocalDateTime.now()
+                    return "[Steam] $personaName 正在游玩 ${cache[flag]}"
                 }
                 // 如果发现开始玩了而之前也在玩
-                gameName != null && cache[playerId] != null -> {
+                gameName != null && cache[flag] != null -> {
                     // 如果发现玩的是新游戏
-                    if (gameName.asString != cache[playerId]) {
-                        cache[playerId] = gameName.asString
-                        return "[Steam] $personaName 正在游玩新游戏 ${cache[playerId]}"
+                    if (gameName.asString != cache[flag]) {
+                        cache[flag] = gameName.asString
+                        gamingTime[flag] = LocalDateTime.now()
+                        return "[Steam] $personaName 正在游玩新游戏 ${cache[flag]}"
                     }
                 }
                 // 之前在玩，现在没玩
-                // TODO: 计算玩了多久
-                gameName == null && cache[playerId] != null -> {
-                    val msg = "[Steam] $personaName 停止游玩 ${cache[playerId]}"
-                    cache.remove(playerId)
+                gameName == null && cache[flag] != null -> {
+                    val time = Duration.between(gamingTime[flag], LocalDateTime.now())
+                    val msg = "[Steam] $personaName 停止游玩 ${cache[flag]} 本次游戏时长 ${time.toMinutes()} 分钟"
+                    cache.remove(flag)
+                    gamingTime.remove(flag)
                     return msg
                 }
             }
@@ -63,8 +71,8 @@ class SteamPlayerStatus {
         log.info("开始处理 Steam 玩家状态订阅")
         cfg.subscriber.forEach { group ->
             log.info("开始处理群：$group，订阅数：${group.value.size}")
-            group.value.forEach { players ->
-                val msg = request(players)
+            group.value.forEach { playerId ->
+                val msg = request(group.toString(), playerId)
                 if (msg.isNotBlank()) bot.sendGroupMsg(group.key.toLong(), msg, false)
             }
         }
